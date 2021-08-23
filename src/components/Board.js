d74d154c82ff2210659ebe3dee76dbdff1bffedb
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-08-23 15:01:10 +0530

    App Finished

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,37 @@
 import React from "react";
 import Square from "./Square";
 
-export default function Board() {
-  return (
-    <div>
-      {/* 1st Row */}
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+export default class Board extends React.Component {
+  renderSquare = (i) => {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        handleClick={() => this.props.clickAction(i)}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
-      {/* 2nd Row */}
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      {/* 3rd Row */}
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </div>
-  );
+    );
+  }
 }
